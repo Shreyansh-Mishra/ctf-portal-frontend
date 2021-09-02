0c80847bab71baca21894ce6ad8e1b2a9202dd4b
batflarrow <dwivedia2001@gmail.com>
2021-09-03 01:58:49 +0530

    remove text field in case of submit

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import './Question.css'
-import axios from 'axios'
+import React from 'react';
+import './Question.css';
+import axios from 'axios';
 import {
     useState,
     useEffect
-} from 'react'
-import { useHistory } from 'react-router-dom'
+} from 'react';
+import { useHistory } from 'react-router-dom';
 export default function Question() {
     let history = useHistory();
-    let [question, setQuestion] = useState(null)
-    let [isPending, setIsPending] = useState(true)
-    let [answer, setAnswer] = useState('')
+    let [question, setQuestion] = useState(null);
+    let [isPending, setIsPending] = useState(true);
+    let [answer, setAnswer] = useState('');
     let [buttonClick, setButtonClick] = useState(0);
     let [status, setStatus] = useState([null]);
 
@@ -21,25 +21,27 @@ export default function Question() {
                 "Authorization": localStorage.getItem("Authorization")
             }
         }).then(res => {
-            if(res.data.success){
-                setQuestion(res.data)
+            if (res.data.success) {
+                setQuestion(res.data);
                 setIsPending(false);
             }
-            else if(res.data.success===false && res.data.msg==="CTF Completed"){
+            else if (res.data.success === false && res.data.msg === "CTF Completed") {
                 setQuestion({
-                    "question":"CTF Completed"
-                })
+                    "question": "CTF Completed"
+                });
                 setIsPending(false);
             }
-        })
-    }
+        });
+    };
 
     useEffect(() => {
         fetchQuestion();
         // eslint-disable-next-line
-    }, [buttonClick, setStatus])
+    }, [buttonClick, setStatus]);
 
     let handleSubmit = () => {
+        if (answer.length === 0 || answer === null)
+            return setStatus([false]);
         axios.post(`${process.env.REACT_APP_BACKEND}user/checkanswer`, {
             'answer': answer
         }, {
@@ -52,54 +54,54 @@ export default function Question() {
                 setStatus([true]);
                 if (buttonClick === 0) {
                     setButtonClick(1);
-                } else { 
+                } else {
                     setButtonClick(0);
                 }
                 setStatus([null]);
-
-            } else if(res.data.success===false&&res.data.error==="You are on Cooldown!") {
-                setStatus(["Cooldown",res.data.waitingTime]);
+                setAnswer('');
+            } else if (res.data.success === false && res.data.error === "You are on Cooldown!") {
+                setStatus(["Cooldown", res.data.waitingTime]);
             }
-            else{
+            else {
                 setStatus([false]);
             }
-        })
-    }
+        });
+    };
 
-if(localStorage.getItem("Authorization")!==null){
-    return (
-    <div className="card-container">
-        <div className="float-layout">
-            <div className="card-image">
-                {/* eslint-disable-next-line*/}
-                {!isPending && !question.image && <img src=""/>}
-                {/* eslint-disable-next-line*/}
-                {!isPending && question.image&&<img src={question.image}/>}
-                <div className="card">
-                    <div className="card-title">{isPending&& <div>Loading...</div>}
-                        {!isPending && question.question}</div><br></br>
-                        {!isPending && question.link &&<a rel="noreferrer" href={question.link} className="submit" style={{"padding":"10px","text-decoration":"none"}} target="_blank">Click Here</a>}<br></br>
-                        {!isPending && question.points && <p>Points: {question.points.toFixed(2)}</p>}
-                    <div className="card-desc">
-                        <center><br></br><br></br><br></br>
-                        {status[0]===true && <p style={{"color": "green"}}>Correct Answer</p>}
-                        {status[0]===false && <p style={{"color":"red"}}>Wrong Answer</p>}
-                        {status[0]==="Cooldown" && <p style={{"color":"yellow"}}>You are on Cooldown! Please wait for {status[1].toFixed(2)} seconds.</p>}
-                        {status[0]===null && <p></p>}
-                            {!isPending && question.question!=="CTF Completed"  &&
-                            <input type="text" className="answer-box" placeholder="Type your answer here..." value={answer} onChange={e=>setAnswer(e.target.value)}></input>}
-                            <br></br><br></br>
-                            {!isPending && question.question!=="CTF Completed"  &&
-                            <input type="submit" className="submit" onClick={handleSubmit}></input>}   
-                        </center>
+    if (localStorage.getItem("Authorization") !== null) {
+        return (
+            <div className="card-container">
+                <div className="float-layout">
+                    <div className="card-image">
+                        {/* eslint-disable-next-line*/}
+                        {!isPending && !question.image && <img src="" />}
+                        {/* eslint-disable-next-line*/}
+                        {!isPending && question.image && <img src={question.image} />}
+                        <div className="card">
+                            <div className="card-title">{isPending && <div>Loading...</div>}
+                                {!isPending && question.question}</div><br></br>
+                            {!isPending && question.link && <a rel="noreferrer" href={question.link} className="submit" style={{ "padding": "10px", "textDecoration": "none" }} target="_blank">Click Here</a>}<br></br>
+                            {!isPending && question.points && <p>Points: {question.points.toFixed(2)}</p>}
+                            <div className="card-desc">
+                                <center><br></br><br></br><br></br>
+                                    {status[0] === true && <p style={{ "color": "green" }}>Correct Answer</p>}
+                                    {status[0] === false && <p style={{ "color": "red" }}>Wrong Answer</p>}
+                                    {status[0] === "Cooldown" && <p style={{ "color": "yellow" }}>You are on Cooldown! Please wait for {status[1].toFixed(2)} seconds.</p>}
+                                    {status[0] === null && <p></p>}
+                                    {!isPending && question.question !== "CTF Completed" &&
+                                        <input type="text" className="answer-box" placeholder="Type your answer here..." value={answer} onChange={e => setAnswer(e.target.value)}></input>}
+                                    <br></br><br></br>
+                                    {!isPending && question.question !== "CTF Completed" &&
+                                        <input type="submit" className="submit" onClick={handleSubmit}></input>}
+                                </center>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-    )
+        );
     }
-    else{
+    else {
         history.push('/login');
         return null;
     }
